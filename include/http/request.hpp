6d9376d3d9a2d2703a12ecd3ce73af0870fad0ac
NCarlisle24 <lorenzonolan12983@gmail.com>
2025-10-28 06:35:02 -0700

    Added HTTP request fields

--- include/http/request.hpp
+++ include/http/request.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <types.h>
+#include <unordered_map>
+
+class HttpRequest {
+    public:
+        char* method; // GET, POST, etc.
+        char* path; // url
+        char* httpVersion;
+
+        std::unordered_map<char*, char*> headers; // keys are case-insensitive
+
+        char* body; // relvant for POST and PUT
+        size_t bodyLength;
+
+        std::unordered_map<char*, char*> queryParams; // ".../?key=value" stuff
+
+        char* clientIp;
+        int clientPort;
+
+        // these are more or less optional
+        char* host;
+        char* contentType;
+        size_t contentLength;
+        int keepAlive;
+
+        HttpRequest(const char* const &httpText);
+        ~HttpRequest();
+};
